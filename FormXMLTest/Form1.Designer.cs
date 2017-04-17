982d074f31d6b0c0a51bc43c66d05b4a4f0717c4
wgoldsmith <winstongoldsmith@hotmail.com>
2017-04-17 09:41:07 -0700

    fixed most Stylecop errors, starting code to place controllers dynamically

--- FormXMLTest/Form1.Designer.cs
+++ FormXMLTest/Form1.Designer.cs
@@ -29,16 +29,13 @@
         private void InitializeComponent()
         {
             this.paperStatGroupBox = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonOk = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton5 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.paperStatGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,10 +43,8 @@
             // 
             this.paperStatGroupBox.Controls.Add(this.radioButton6);
             this.paperStatGroupBox.Controls.Add(this.radioButton5);
-            this.paperStatGroupBox.Controls.Add(this.radioButton4);
             this.paperStatGroupBox.Controls.Add(this.radioButton3);
             this.paperStatGroupBox.Controls.Add(this.radioButton2);
-            this.paperStatGroupBox.Controls.Add(this.radioButton1);
             this.paperStatGroupBox.Controls.Add(this.label1);
             this.paperStatGroupBox.Location = new System.Drawing.Point(12, 12);
             this.paperStatGroupBox.Name = "paperStatGroupBox";
@@ -57,6 +52,48 @@
             this.paperStatGroupBox.TabIndex = 0;
             this.paperStatGroupBox.TabStop = false;
             this.paperStatGroupBox.Text = "Paper Status";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(252, 68);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(85, 17);
+            this.radioButton6.TabIndex = 6;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(252, 44);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(85, 17);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(88, 68);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(88, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -84,82 +121,13 @@
             this.buttonCancel.TabIndex = 2;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(88, 20);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(88, 44);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(88, 68);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(85, 17);
-            this.radioButton3.TabIndex = 3;
-            this.radioButton3.Text = "radioButton3";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(252, 20);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(85, 17);
-            this.radioButton4.TabIndex = 4;
-            this.radioButton4.Text = "radioButton4";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // radioButton5
-            // 
-            this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(252, 44);
-            this.radioButton5.Name = "radioButton5";
-            this.radioButton5.Size = new System.Drawing.Size(85, 17);
-            this.radioButton5.TabIndex = 5;
-            this.radioButton5.Text = "radioButton5";
-            this.radioButton5.UseVisualStyleBackColor = true;
-            // 
-            // radioButton6
-            // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(252, 68);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(85, 17);
-            this.radioButton6.TabIndex = 6;
-            this.radioButton6.Text = "radioButton6";
-            this.radioButton6.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Location = new System.Drawing.Point(49, 148);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 7;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(344, 261);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOk);
             this.Controls.Add(this.paperStatGroupBox);
@@ -180,11 +148,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RadioButton radioButton6;
         private System.Windows.Forms.RadioButton radioButton5;
-        private System.Windows.Forms.RadioButton radioButton4;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- FormXMLTest/Form1.cs
+++ FormXMLTest/Form1.cs
@@ -1,41 +1,82 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Xml;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Form1.cs" company="Goldsmith Engineering">
+//     Copyright (c) Goldsmith Engineering. All rights reserved.
+// </copyright>
+// <author>Winston Goldsmith</author>
+//-----------------------------------------------------------------------
 
 namespace FormXMLTest
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+    using System.Xml;
+
+    /// <summary>
+    /// Initializes form, reads in data from XML file and displays it on the form.
+    /// </summary>
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-            ReadXML();
-        }
-
+        /// <summary>
+        /// A list to hold all the Panels on the form that show different colors based on what type of paper is selected
+        /// </summary>
         private Dictionary<int, Panel> colorBoxList = new Dictionary<int, Panel>();
+
+        /// <summary>
+        /// A list to hold all the Panels on the form that hold all the controllers that must be placed dynamically based on the XML file
+        /// </summary>
+        private Dictionary<int, Panel> containerList = new Dictionary<int, Panel>();
+
+        /// <summary>
+        /// A list to hold all the Labels on the form that show name of each printer
+        /// </summary>
         private Dictionary<int, Label> printerList = new Dictionary<int, Label>();
+
+        /// <summary>
+        /// A list to hold all the RadioButtons on the form for selecting the mylar option for that printer
+        /// </summary>
         private Dictionary<int, RadioButton> mylarList = new Dictionary<int, RadioButton>();
+
+        /// <summary>
+        /// A list to hold all the RadioButtons on the form for selecting the paper option for that printer
+        /// </summary>
         private Dictionary<int, RadioButton> paperList = new Dictionary<int, RadioButton>();
+
+        /// <summary>
+        /// A list to hold all the RadioButtons on the form for selecting the other option for that printer
+        /// </summary>
         private Dictionary<int, RadioButton> otherList = new Dictionary<int, RadioButton>();
+
+        /// <summary>
+        /// A list to hold all the Labels on the form that show the name of the last person to change the option on that printer, 
+        /// as well as the date and time it was changed
+        /// </summary>
         private Dictionary<int, Label> ndtList = new Dictionary<int, Label>();
 
+        /// <summary>
+        /// Initializes a new instance of the Form1 class. Auto generated constructor for Form1
+        /// </summary>
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
 
+        /// <summary>
+        /// Reads the plot printers xml file and creates controllers based on the information in the file
+        /// </summary>
         private void ReadXML()
         {
-
             int key = 0;
             ///////////////////////////////////////////////////////////////////////               Change to new file location              //////////////////////////////////////////////
             /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            using (XmlReader reader = XmlReader.Create(@"C:\Users\wgoldsmith\Documents\Visual Studio 2015\Projects\Plot-Command-CS\Plot-Command-CS\plotprinters.xml"))
+            using (XmlReader reader = XmlReader.Create(@"K:\common\Winston_Goldsmith\plotprinters.xml"))
             { 
-
                 while (reader.Read())
                 {                
                     switch (reader.NodeType)
@@ -44,66 +85,151 @@ namespace FormXMLTest
 
                             if (reader.Name.Equals("printer"))
                             {
-                                    
-                                printerList.Add(key, new Label());// reader.ReadString());
-                                printerList[key].Text = reader.GetAttribute("name");
-                                printerList[key].Location = new Point(5, 56);
-
-                                printerList[key].AutoSize = true;
-                                printerList[key].Name = "label1";
-                                    
+                                this.containerList.Add(key, new Panel());
+                                this.printerList.Add(key, new Label());
+                                this.printerList[key].Text = reader.GetAttribute("name");
                             }
-
-                            else if(reader.Name.Equals("user"))
+                            else if (reader.Name.Equals("user"))
                             {
-                                ndtList.Add(key, new Label());
-                                ndtList[key].Text = reader.ReadElementContentAsString();
-                                ndtList[key].Location = new Point(150, 100);
-
-                                ndtList[key].AutoSize = true;
-                                ndtList[key].Name = "label1";
+                                this.ndtList.Add(key, new Label());
+                                this.ndtList[key].Text = "Last changed by: " + reader.ReadElementContentAsString();
                             }
-                                
+
+                            // month and hour are both preceded by " - "
+                            else if (reader.Name.Equals("month") || reader.Name.Equals("hour"))
+                            {
+                                this.ndtList[key].Text = this.ndtList[key].Text + " - " + reader.ReadElementContentAsString();
+                            }
+                            else if (reader.Name.Equals("day") || reader.Name.Equals("year"))
+                            {
+                                this.ndtList[key].Text = this.ndtList[key].Text + "/" + reader.ReadElementContentAsString();
+                            }
+                            else if (reader.Name.Equals("minute"))
+                            {
+                                this.ndtList[key].Text = this.ndtList[key].Text + ":" + reader.ReadElementContentAsString();
+                            }
+                            else if (reader.Name.Equals("roll"))
+                            {
+                                this.colorBoxList.Add(key, new Panel());
+                                this.colorBoxList[key].BorderStyle = BorderStyle.Fixed3D;
+                                this.colorBoxList[key].Size = new Size(35, 25);
+
+                                this.mylarList.Add(key, new RadioButton());
+                                this.mylarList[key].Text = "Mylar";
+
+                                this.paperList.Add(key, new RadioButton());
+                                this.paperList[key].Text = "Paper";
+
+                                this.otherList.Add(key, new RadioButton());
+                                this.otherList[key].Text = "Other";
+
+                                SetRoll(reader.ReadElementContentAsString(), key);
+                            }
+
                             break;
 
-                        case XmlNodeType.EndElement:  //  when it reaches the end of a printer, increase key by 1
+                        case XmlNodeType.EndElement:  // when it reaches the end of a printer, increase key by 1
                             if (reader.Name.Equals("printer"))
+                            {
                                 key++;
+                            }
 
                             break;
-
-                    }
-                    
+                    }                   
                 }
             }
 
-            paperStatGroupBox.Height = 41 * key + 15;
+            this.paperStatGroupBox.Height = (41 * key) + 15;
         }
 
+        /// <summary>
+        /// Reads the plot printers xml file and creates controllers based on the information in the file
+        /// </summary>
+        /// <param name="roll">The name of the roll that was read from the XML file.</param>
+        /// <param name="key">The number of the current printer.</param>
+        private void SetRoll(string roll, int key)
+        {
+            if (roll.Equals("Mylar"))
+            {
+                this.colorBoxList[key].BackColor = Color.Red;
+                this.mylarList[key].Checked = true;
+            }
+            else if (roll.Equals("Paper"))
+            {
+                this.colorBoxList[key].BackColor = Color.White;
+                this.paperList[key].Checked = true;
+            }
+            else if (roll.Equals("Other"))
+            {
+                this.colorBoxList[key].BackColor = Color.Green;
+                this.otherList[key].Checked = true;
+            }
+        }
+
+        private void PlaceContainerPanel(int key)
+        {
+
+        }
+
+        private void PlaceInContainer(int key)
+        {
+            
+        }
+
+        /// <summary>
+        /// Runs when form is loaded. Loads all controllers and adds them to form based on what is in plot printer xml file.
+        /// </summary>
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.ReadXML();
+
             Label temp = new Label();
 
-            temp.Location = new Point(5, 15);
+            temp.Location = new Point(25, 15);
             temp.Name = "temp";
             temp.TabIndex = 1;
-            temp.Text = "tempttt";// + printerList[1].Text;
-            if (ndtList.ContainsKey(1))
-                temp.Text = ">>>" + ndtList[1].Text + "<<<";
+            temp.Text = "tempttt"; // + printerList[1].Text;
+            if (this.ndtList.ContainsKey(1))
+            {
+                temp.Text = ">>>" + this.ndtList[1].Text + "<<<";
+            }
            
             temp.AutoSize = true;
 
-            paperStatGroupBox.Controls.Add(temp);
-            paperStatGroupBox.Controls.Add(printerList[0]);
+            this.printerList[1].Location = new Point(5, 56);
 
-            //                           keep same X location,     top of groupBox + height = bottom of box. + 20 for some space between box and button.
-            buttonOk.Location = new Point(buttonOk.Location.X, paperStatGroupBox.Location.Y + paperStatGroupBox.Height + 20);
-            buttonCancel.Location = new Point(buttonCancel.Location.X, paperStatGroupBox.Location.Y + paperStatGroupBox.Height + 20);
+            this.paperStatGroupBox.Controls.Add(temp);
+            this.paperStatGroupBox.Controls.Add(this.printerList[1]);
 
+            // interate through container panels, place them in group box, and place controllers inside.
+            // if there are no container panels, nothing will happen.
+            for (int key = 0; key < this.containerList.Count; key++)
+            {
+                PlaceContainerPanel(key);
+                PlaceInContainer(key);
+            }
 
+            // -------------------------------------keep same X location,     top of groupBox           +         height = bottom of box. + 20 for some space between box and button.
+            this.buttonOk.Location = new Point(this.buttonOk.Location.X, this.paperStatGroupBox.Location.Y + this.paperStatGroupBox.Height + 20);
+            this.buttonCancel.Location = new Point(this.buttonCancel.Location.X, this.paperStatGroupBox.Location.Y + this.paperStatGroupBox.Height + 20);
+
+            RadioButton radBut = new RadioButton();
+            radBut.Text = "Stuff";
+            radBut.Checked = true;
+            this.paperStatGroupBox.Controls.Add(radBut);
 
             label1.Text = DateTime.Now.Hour.ToString();
             label1.AutoSize = true;
+        }
+
+        /// <summary>
+        /// When user clicks cancel button, close form.
+        /// </summary>
+        /// <param name="sender">Auto generated sender object by Visual Studio.</param>
+        /// <param name="e">Auto generated EventArgs by Visual Studio.</param>
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
